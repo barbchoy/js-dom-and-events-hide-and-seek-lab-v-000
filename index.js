1d5c80fcbc0da37eb668a6e50309a5549b901a14
github username <github email address>
2019-04-29 13:07:08 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild() {
+  let node = document.getElementById( 'grand-node' )
+  let nextNode = node.children[ 0 ]
+
+  while ( nextNode ) {
+    node = nextNode
+    nextNode = node.children[ 0 ]
+  }
+  return node
+}
+
+function increaseRankBy(n){
+  const nodes = document.querySelectorAll('.ranked-list');
+  for (let i = 0; i < nodes.length; i++){
+    let children = nodes[i].children;
+    for (let j = 0; j < children.length; j++){
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+}
